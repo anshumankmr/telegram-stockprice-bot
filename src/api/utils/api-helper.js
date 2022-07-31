4bf3b96e240afb8096a62fdb3cb5ee4b664dbe14
Anshuman Kumar <anshumankmr80@gmail.com>
2022-07-31 14:34:27 +0530

    Revert "Update api helper to remove retry logic"
    This reverts commit d29924984f07f8e77efa2a0466df0a9e94a6e678.

--- src/api/utils/api-helper.js
+++ src/api/utils/api-helper.js
@@ -1,8 +1,27 @@
 const logger = require('../../config/logger');
 const axios = require('axios');
 
+const retryWrapper = (axios, options) => {
+	const max_time = options.retry_time;
+	let counter = 0;
+	axios.interceptors.response.use(null,async (error) => {
+		const config = error.config;
+		const { saveAccessToken } = require('../services/kite/save-access-token');
+		logger.log('info','API Call Failed');
+		if (counter < max_time && error.response.status !== 200) {
+			counter++;
+			await saveAccessToken();
+			return new Promise((resolve) => {
+				resolve(axios(config));
+			});
+		}
+		return Promise.reject(error);
+	});
+};
+
 module.exports.call = (requestOptions, requestHeaders = {}) => {
 	try{
+		retryWrapper(axios, {retry_time: 1});
 		const options = Object.assign({},requestOptions);
 		options.json = true;
 		options.headers = requestHeaders;
